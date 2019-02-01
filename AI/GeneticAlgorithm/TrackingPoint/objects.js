9f673ee9c491b24a95882bce4097a85833f380c7
Philipp <tisciit@gmail.com>
2019-02-01 11:36:14 +0100

    TrackingPoint: Added Generation object to wrap multiple point objects; added random vectors to brain

--- AI/GeneticAlgorithm/TrackingPoint/objects.js
+++ AI/GeneticAlgorithm/TrackingPoint/objects.js
@@ -2,46 +2,94 @@ class Point {
 
     constructor(x, y, size) {
         this.size = size;
-        this.position;
-        this.velocity;
-        this.acceleration;
+        this.position = createVector(x, y);
+        this.velocity = createVector();;
+        this.acceleration = createVector();
         this.alive = true;
-        this.brain = new Brain();
+        this.brain = new Brain(400);
+    }
+
+
+    draw() {
+        ellipse(this.position.x, this.position.y, this.size);
     }
 
     move() {
+        this.acceleration = this.brain.getNext();
         this.velocity.add(this.acceleration);
         this.velocity.limit(5);
         this.position.add(this.velocity);
     }
 
     checkCollision(obstacles) {
-        for (let o of obstacles) {
-            if (o.p1.x <= this.position.x &&
-                o.p2.x >= this.position.x &&
-                o.p1.y <= this.position.y &&
-                o.p2.y >= this.position.y) {
-                return true;
+        if (obstacles) {
+            for (let o of obstacles) {
+                if (o.p1.x <= this.position.x &&
+                    o.p2.x >= this.position.x &&
+                    o.p1.y <= this.position.y &&
+                    o.p2.y >= this.position.y) {
+                    return true;
+                }
             }
         }
         return false;
     }
 
-    update() {
+    update(obstacles) {
         if (this.alive) {
             this.move();
-            this.alive = !this.checkCollision();
+            this.alive = !this.checkCollision(obstacles);
+
+            if (!this.alive) {
+                console.log("im ded");
+                this.velocity = createVector();
+                this.acceleration = createVector();
+            }
         }
     }
 
 }
 
 class Brain {
+    constructor(instructions) {
+        this.instructions = [];
+        this.step = 0;
 
+        for (let i = 0; i < instructions; i++) {
+            this.instructions[i] = p5.Vector.random2D();
+        }
+    }
+
+    getNext() {
+        if (this.step == this.instructions.length) {
+            return createVector(0, 0);
+        }
+        let ret = this.instructions[this.step];
+        this.step++;
+        return ret;
+    }
 }
 
 class Generation {
+    constructor(members) {
+        this.points = [];
 
+        for (let i = 0; i < members; i++) {
+            this.points.push(new Point(width / 2, height * .8, 10));
+        }
+    }
+
+    draw() {
+        for(let p of this.points){
+            p.draw();
+        }
+    }
+
+    update(obstacles) {
+        for (let p of this.points) {
+            p.update(obstacles);
+        }
+    }
 }
 
 class Obstacle {
@@ -49,5 +97,13 @@ class Obstacle {
         this.color = c;
         this.p1 = createVector(x1, y1);
         this.p2 = createVector(x2, y2);
+    }
+
+    draw() {
+        push();
+        rectMode(CORNERS);
+        fill(this.color);
+        rect(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+        pop();
     }
 }
--- AI/GeneticAlgorithm/TrackingPoint/sketch.js
+++ AI/GeneticAlgorithm/TrackingPoint/sketch.js
@@ -1,12 +1,25 @@
-let point;
+let generation;
 let obstacles = [];
 
 function setup() {
-
+    const OBSTACLESIZE = 30;
     createCanvas(400, 600);
     background(52);
+
+    obstacles.push(new Obstacle(color(200), 0, 0, OBSTACLESIZE, height));
+    obstacles.push(new Obstacle(color(200), width - OBSTACLESIZE, 0, width, height));
+
+    obstacles.push(new Obstacle(color(200), 0, 0, width, OBSTACLESIZE));
+    obstacles.push(new Obstacle(color(200), 0, height - OBSTACLESIZE, width, height));
+
+    generation = new Generation(20);
 }
 
 function draw() {
-
+    background(52);
+    for (let o of obstacles) {
+        o.draw();
+    }
+    generation.draw();
+    generation.update(obstacles);
 }
