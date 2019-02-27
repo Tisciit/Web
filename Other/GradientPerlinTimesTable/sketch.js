c0b2b96bf4ad8de504f40560dd06b02c9d93e3b6
Tisciit <38668741+Tisciit@users.noreply.github.com>
2019-02-27 17:23:44 +0100

    Add files via upload

--- Other/GradientPerlinTimesTable/sketch.js
+++ Other/GradientPerlinTimesTable/sketch.js
@@ -1,23 +1,24 @@
-let numberOfPoints = 300;
+let numberOfPoints = 3;
 let radius = 200;
-let connectBy = 80;
+let connectBy = 0;
 let noiseAngle = 0;
 
 function setup() {
     createCanvas(800, 800);
     background(0);
-    radius = width / 2 - 10
+    radius = width / 2 - 30
 
     noFill();
-    frameRate(30);
+    frameRate(20);
 }
 
 function draw() {
     translate(width / 2, height / 2);
 
-    background(0, 50);
-    stroke(200);
+    background(20, 200);
+
     //Outter Ring
+    stroke(200);
     ellipse(0, 0, radius * 2);
 
     //Draw Point riding the outter ring
@@ -27,61 +28,57 @@ function draw() {
 
     //Do perlin noise smooth transitions
     let n = noise(cos(noiseAngle), sin(noiseAngle));
-    numberOfPoints = mapNoiseValue(n, numberOfPoints, 10, 200);
-    connectBy = mapNoiseValue(n, connectBy, 1, 10);
+    numberOfPoints = mapNoiseValue(n, numberOfPoints, 100, 1000);
+    connectBy += .001;
 
     //draw gradient lines for each connection
     strokeWeight(1);
 
     for (let i = 0; i < numberOfPoints; i++) {
-
         //define colors
-        let c1 = color(125, 249, 255, noise(noiseAngle) * 255);
-        let c2 = color(207, 16, 32, noise(noiseAngle + 1) * 255);
+        let c1 = color(125, 249, 255, 140);
+        let c2 = color(207, 16, 32, 140);
 
         //find points
-        let p1 = getPoint(i, radius);
-        let p2 = getPoint(i * connectBy, radius);
+        let p1 = getPoint(i, radius, noiseAngle);
+        let p2 = getPoint(i * connectBy, radius, noiseAngle);
 
         //draw Points
-        gradientLine(p1, p2, c1, c2, 6);
+        p1.gradientLineTo(p2.x, p2.y, c1, c2, 4);
     }
 
 
     //increment the angle in perlin noise space
-    noiseAngle += radians(.5);
+    noiseAngle += radians(1);
     if (noiseAngle >= TWO_PI) {
         noiseAngle = 0;
-        //background(0);
     }
 
 }
 
-p5.prototype.gradientLine = function (vector1, vector2, color1, color2, slices = 5) {
+p5.Vector.prototype.gradientLineTo = function (targetX, targetY, color1, color2, slices = 5) {
+    gradientLine(this.x, this.y, targetX, targetY, color1, color2, slices);
+}
 
-    const lengthOfSlice = dist(vector1.x, vector1.y, vector2.x, vector2.y) / slices;
-    const angle = atan2(vector2.y - vector1.y, vector2.x - vector1.x);
+p5.prototype.gradientLine = function (x1, y1, x2, y2, color1, color2, slices = 5) {
 
-    let p0 = createVector(vector1.x, vector1.y);
+    push();
+    const lengthOfSlice = dist(x1, y1, x2, y2) / slices;
+    const angle = atan2(y2 - y1, x2 - x1);
+    //const moveVector = createVector(x1, y1);
 
     //do once for each slice of the gradient line
     for (let i = 0; i < slices; i++) {
-        let inter = map(i, 0, slices, 0, 1);
-        let c = lerpColor(color1, color2, inter);
-
-        vector1.x += lengthOfSlice * cos(angle);
-        vector1.y += lengthOfSlice * sin(angle);
-        push();
-        noFill();
+        const inter = map(i, 0, slices, 0, 1);
+        const c = lerpColor(color1, color2, inter);
         stroke(c);
-        beginShape();
-        vertex(p0.x, p0.y);
-        vertex(vector1.x, vector1.y);
-        endShape();
-        pop();
 
-        p0 = createVector(vector1.x, vector1.y);
+        line(x1,
+            y1,
+            x1 += lengthOfSlice * cos(angle),
+            y1 += lengthOfSlice * sin(angle));
     }
+    pop();
 }
 
 function mapNoiseValue(noise, value, min, max) {
@@ -95,8 +92,8 @@ function mapNoiseValue(noise, value, min, max) {
     return value;
 }
 
-function getPoint(index, radius) {
-    let angle = TWO_PI / numberOfPoints * (index % numberOfPoints) + PI / 2 + noiseAngle;
+function getPoint(index, radius, phase) {
+    let angle = TWO_PI / numberOfPoints * (index % numberOfPoints) + PI / 2 + phase;
     let x = radius * cos(angle);
     let y = radius * sin(angle);
 
